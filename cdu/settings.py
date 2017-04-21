f7b2bb0d4bc2d4b08452a91c7abcd38c8f0acf27
Brian Jinwright <bjinwright@qwigo.com>
2017-04-21 14:47:11 -0400

    updated string imports in cdu.settings to django_warrant

--- cdu/settings.py
+++ cdu/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 AUTHENTICATION_BACKENDS = [
-    'warrant.django.backend.CognitoBackend',
+    'django_warrant.backend.CognitoBackend',
     'django.contrib.auth.backends.ModelBackend'
 ]
 
@@ -65,7 +65,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'warrant.django',
+    'django_warrant',
     'crispy_forms',
     'django_extensions'
 ]
