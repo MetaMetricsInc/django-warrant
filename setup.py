eda973e423927966f17ac5e030bc06063a3573fe
Brian Jinwright <bjinwright@qwigo.com>
2017-04-21 14:56:31 -0400

    added setup.py and add .idea/ to the gitignore

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt', session=False)
+
+version = '0.1.0'
+
+README="""Python class to integrate Boto3's Cognito client so it is easy to login users. With SRP support."""
+
+setup(
+    name='warrant',
+    version=version,
+    description=README,
+    long_description=README,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Environment :: Web Environment",
+    ],
+    keywords='aws,cognito,api,gateway,capless',
+    author='Capless.io',
+    author_email='opensource@capless.io',
+    maintainer='Brian Jinwright',
+    packages=find_packages(exclude=('cdu',)),
+    url='https://github.com/capless/warrant',
+    license='GNU GPL V3',
+    install_requires=[str(ir.req) for ir in install_reqs],
+    include_package_data=True,
+    zip_safe=True,
+
+)
